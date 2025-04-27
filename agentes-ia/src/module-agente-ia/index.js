f68f8ac12eda99dcf310db04bbbdaa674f275b86
andrilconsultoria <155909486+andriltec@users.noreply.github.com>
2025-04-27 16:43:17 -0400

    Create index.js

---
+++ agentes-ia/src/module-agente-ia/index.js
@@ -0,0 +1,13 @@
+import ModuleComponent from './module.vue';
+
+export default {
+	id: 'agentes-ia',
+	name: 'Agentes de IA',
+	icon: 'box',
+	routes: [
+		{
+			path: '',
+			component: ModuleComponent,
+		},
+	],
+};
