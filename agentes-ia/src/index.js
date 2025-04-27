770bfc4299532897554b0c6ed08f1e04fb2e0f8b
andrilconsultoria <155909486+andriltec@users.noreply.github.com>
2025-04-27 16:45:05 -0400

    Add files via upload

---
+++ agentes-ia/src/index.js
@@ -0,0 +1,8 @@
+import moduleAgenteIA from './module-agente-ia/index.js';
+
+export default {
+  id: 'directus-extension-bundle-agent',
+  name: 'Agente IA Bundle',
+  description: 'Extensão bundle para painéis do agente IA.',
+  modules: [moduleAgenteIA]
+};
